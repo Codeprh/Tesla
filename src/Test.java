7b36cffe416965beb02d7b9831ba7af4085edb68
Codeprh <Codingprh@gmail.com>
2018-08-19 21:25:19 +0800

    第十一题

--- src/Test.java
+++ src/Test.java
@@ -1,2 +1,9 @@
 public class Test {
+
+    public static void main(String[] args) {
+        String st2 = "1java";
+        String str1 = new StringBuilder("1").append("java").toString();
+        System.out.println(str1.intern() == str1);
+
+    }
 }
---
+++ src/swordFingerOffer/d11_IntCount1Binary.java
@@ -0,0 +1,13 @@
+package swordFingerOffer;
+
+/**
+ * 二进制中 1 的个数
+ * <p>
+ * 输入一个整数，输出该数二进制表示中 1 的个数。
+ */
+public class d11_IntCount1Binary {
+    public static void main(String[] args) {
+        //方法1：使用Integer的原生方法
+        System.out.println(Integer.bitCount(15));
+    }
+}
