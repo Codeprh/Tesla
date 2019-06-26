919f7f377b8428b2e2507af20681a4d97625af73
PanRunHuan <Codingprh@gmail.com>
2019-06-26 13:43:23 +0800

    二叉查找树的第k大值：中序遍历

--- src/leetcode/_230.java
+++ src/leetcode/_230.java
@@ -1,5 +1,10 @@
 package leetcode;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Stack;
+
 /**
  * @author codingprh
  * @create 2019-06-25 17:54
@@ -21,8 +26,52 @@ public class _230 {
         }
     }
 
-    public static void main(String []args){
+    public int kthSmallest(TreeNode root, int k) {
+        //寻找第k小的元素:中序遍历一个二叉查找树（BST）的结果是一个有序数组，
+        //4275136
+        Stack<TreeNode> stack = new Stack<>();
+        List<Integer> re = new ArrayList<>();
+        //入栈操作
+        while (true) {
+            while (root != null) {
+                stack.push(root);
 
+                root = root.left;
+            }
+
+            root = stack.pop();
+
+            re.add(root.Data);
+            System.out.print(root.Data);
+
+            root = root.right;
+        }
+    }
+
+    public int kthSmallest_v2(TreeNode root, int k) {
+        LinkedList<TreeNode> stack = new LinkedList<TreeNode>();
+
+        while (true) {
+            while (root != null) {
+                stack.add(root);
+                root = root.left;
+            }
+            root = stack.removeLast();
+            System.out.print(root.Data);
+            if (--k == 0) return root.Data;
+            root = root.right;
+        }
+    }
+
+
+    public static void main(String[] args) {
+        _230 start = new _230();
+        TreeNode root = start.createTree();
+        System.out.println(start.kthSmallest(root, 7));
+
+    }
+
+    public void print(TreeNode head) {
 
     }
 
@@ -36,13 +85,13 @@ public class _230 {
 
     private TreeNode createTree() {
         /**
-         *         //            1
-         *         //         /      \
-         *         //        2        3
-         *         //       /\         \
-         *         //      4  5         6
-         *         //        /
-         *         //       7
+         *        5
+         *       / \
+         *      3   6
+         *     / \
+         *    2   4
+         *   /
+         *  1
          */
         TreeNode node1 = new TreeNode(1);
         TreeNode node2 = new TreeNode(2);
