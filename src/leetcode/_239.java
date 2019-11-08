e808755d62b9ed92541e3862afe09dbe85c2f814
noah <codingprh@gmail.com>
2019-11-08 10:10:20 +0800

    代码提交

---
+++ src/leetcode/_239.java
@@ -0,0 +1,107 @@
+package leetcode;
+
+import java.util.ArrayDeque;
+import java.util.Arrays;
+
+/**
+ * 描述:
+ * 滑动窗口找最大值
+ * <p>
+ * 给定一个数组 nums，有一个大小为 k 的滑动窗口从数组的最左侧移动到数组的最右侧。你只可以看到在滑动窗口内的 k 个数字。滑动窗口每次只向右移动一位。
+ * <p>
+ * 返回滑动窗口中的最大值。
+ * <p>
+ *
+ * @author Noah
+ * @create 2019-11-07 17:08
+ */
+public class _239 {
+
+    /**
+     * 参考版本-最佳
+     * 输入: nums = [1,3,-1,-3,5,3,6,7], 和 k = 3
+     * 输出: [3,3,5,5,6,7]
+     *
+     * @param nums
+     * @param k
+     * @return
+     */
+    public int[] maxSlidingWindow(int[] nums, int k) {
+        int n = nums.length;
+        if (n * k == 0) return new int[0];
+        if (k == 1) return nums;
+
+        int[] left = new int[n];
+        left[0] = nums[0];
+        int[] right = new int[n];
+        right[n - 1] = nums[n - 1];
+        for (int i = 1; i < n; i++) {
+            // from left to right
+            if (i % k == 0) left[i] = nums[i];  // block_start
+            else left[i] = Math.max(left[i - 1], nums[i]);
+
+            // from right to left
+            int j = n - i - 1;
+            if ((j + 1) % k == 0) right[j] = nums[j];  // block_end
+            else right[j] = Math.max(right[j + 1], nums[j]);
+        }
+
+        int[] output = new int[n - k + 1];
+        for (int i = 0; i < n - k + 1; i++)
+            output[i] = Math.max(left[i + k - 1], right[i]);
+
+        return output;
+
+    }
+
+    /**
+     * todo：考虑堆实现
+     * 参考版本-双端队列实现
+     */
+    ArrayDeque<Integer> deq = new ArrayDeque<Integer>();
+    int[] nums;
+
+    public void clean_deque(int i, int k) {
+        // remove indexes of elements not from sliding window
+        if (!deq.isEmpty() && deq.getFirst() == i - k)
+            deq.removeFirst();
+
+        // remove from deq indexes of all elements
+        // which are smaller than current element nums[i]
+        while (!deq.isEmpty() && nums[i] > nums[deq.getLast()]) deq.removeLast();
+    }
+
+    public int[] maxSlidingWindow_v2(int[] nums, int k) {
+        int n = nums.length;
+        if (n * k == 0) return new int[0];
+        if (k == 1) return nums;
+
+        // init deque and output
+        this.nums = nums;
+        int max_idx = 0;
+        for (int i = 0; i < k; i++) {
+            clean_deque(i, k);
+            deq.addLast(i);
+            // compute max in nums[:k]
+            if (nums[i] > nums[max_idx]) max_idx = i;
+        }
+        int[] output = new int[n - k + 1];
+        output[0] = nums[max_idx];
+
+        // build output
+        for (int i = k; i < n; i++) {
+            clean_deque(i, k);
+            deq.addLast(i);
+            output[i - k + 1] = nums[deq.getFirst()];
+        }
+        return output;
+    }
+
+    public static void main(String[] args) {
+        _239 app = new _239();
+        int[] a = new int[]{1, 3, -1, -3, 5, 3, 6, 7};
+        System.out.println(Arrays.toString(app.maxSlidingWindow_v2(a, 3)));
+    }
+
+
+}
