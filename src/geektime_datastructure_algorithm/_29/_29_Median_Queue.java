4edc23fc2cddf3fbb4d2c24507b0fd714bcd837e
noah <codingprh@gmail.com>
2019-11-20 18:14:41 +0800

    中位数代码提交最终版

---
+++ src/geektime_datastructure_algorithm/_29/_29_Median_Queue.java
@@ -0,0 +1,51 @@
+package geektime_datastructure_algorithm._29;
+
+import java.util.PriorityQueue;
+
+/**
+ * 描述:
+ * 求中位数使用优先级队列实现(堆)
+ *
+ * @author Noah
+ * @create 2019-11-20 18:04
+ */
+public class _29_Median_Queue {
+
+
+    /**
+     * 当前大顶堆和小顶堆的元素个数之和
+     */
+    private int count;
+    private PriorityQueue<Integer> maxheap;
+    private PriorityQueue<Integer> minheap;
+
+    /**
+     * initialize your data structure here.
+     */
+    public _29_Median_Queue() {
+        count = 0;
+        maxheap = new PriorityQueue<>((x, y) -> y - x);
+        minheap = new PriorityQueue<>();
+    }
+
+    public void addNum(int num) {
+        count += 1;
+        maxheap.offer(num);
+        minheap.add(maxheap.poll());
+        // 如果两个堆合起来的元素个数是奇数，小顶堆要拿出堆顶元素给大顶堆
+        if ((count & 1) != 0) {
+            maxheap.add(minheap.poll());
+        }
+    }
+
+    public double findMedian() {
+        if ((count & 1) == 0) {
+            // 如果两个堆合起来的元素个数是偶数，数据流的中位数就是各自堆顶元素的平均值
+            return (double) (maxheap.peek() + minheap.peek()) / 2;
+        } else {
+            // 如果两个堆合起来的元素个数是奇数，数据流的中位数大顶堆的堆顶元素
+            return (double) maxheap.peek();
+        }
+    }
+
+}
