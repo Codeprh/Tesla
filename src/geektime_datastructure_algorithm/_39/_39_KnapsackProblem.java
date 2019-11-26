0ec496e4c7045aa5dcd422093fe3886ec674ad19
Noah Pan <codingprh@gmail.com>
2019-11-26 09:17:12 +0800

    代码提交

--- src/geektime_datastructure_algorithm/_39/_39_KnapsackProblem.java
+++ src/geektime_datastructure_algorithm/_39/_39_KnapsackProblem.java
@@ -28,7 +28,7 @@ public class _39_KnapsackProblem {
         System.out.println("最大重量=" + app.maxW);
 
         _39_KnapsackProblem_Simple app1 = new _39_KnapsackProblem_Simple();
-        app1.f(0, 0);
+        app1.f2(0, 0);
         System.out.println("最大重量=" + app1.maxW);
     }
 
@@ -70,7 +70,14 @@ public class _39_KnapsackProblem {
         private int[] weight = new int[]{6, 1, 1, 1, 3}; // 物品重量
         private int n = 5; // 物品个数
         private int w = 9; // 背包承受的最大重量
+        private boolean[][] mem = new boolean[5][10]; // 备忘录，默认值false
 
+        /**
+         * 常规方法：存在冗余计算
+         *
+         * @param i
+         * @param cw
+         */
         public void f(int i, int cw) { // 调用f(0, 0)
             if (cw == w || i == n) { // cw==w表示装满了，i==n表示物品都考察完了
 
@@ -85,5 +92,24 @@ public class _39_KnapsackProblem {
 
         }
 
+        /**
+         * 优化版本：使用数组存储临时值
+         *
+         * @param i
+         * @param cw
+         */
+        public void f2(int i, int cw) { // 调用f(0, 0)
+            if (cw == w || i == n) { // cw==w表示装满了，i==n表示物品都考察完了
+                if (cw > maxW) maxW = cw;
+                return;
+            }
+            if (mem[i][cw]) return; // 重复状态
+            mem[i][cw] = true; // 记录(i, cw)这个状态
+            f2(i + 1, cw); // 选择不装第i个物品
+            if (cw + weight[i] <= w) {
+                f2(i + 1, cw + weight[i]); // 选择装第i个物品
+
+            }
+        }
     }
 }
