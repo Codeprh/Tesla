b5bc2463e70bd1f51df3d25eb1061df39a992200
Noah Pan <codingprh@gmail.com>
2019-11-28 16:18:57 +0800

    代码提交

--- src/leetcode/_23.java
+++ src/leetcode/_23.java
@@ -22,7 +22,8 @@ public class _23 {
     public static void main(String[] args) {
         _23 app = new _23();
 
-        ListNode head = app.mergeKLists(app.createListNodeArr());
+        //ListNode head = app.mergeKLists(app.createListNodeArr());
+        app.print(app.mergeKLists_v3(app.createListNodeArr()));
 
     }
 
@@ -84,15 +85,90 @@ public class _23 {
     }
 
     /**
+     * 自己版本
      * 分治思想：两两合并
      *
      * @param lists
      * @return
      */
     public ListNode mergeKLists_v2(ListNode[] lists) {
+
         ListNode head = new ListNode(0);
         ListNode p = head;
-        return null;
+
+        ListNode head2 = mergeTwoLists(lists[0], lists[1]);
+
+        for (int i = 2; i < lists.length; i++) {
+
+        }
+
+        return head;
+    }
+
+    /**
+     * 参考版：分治算法思想来解决
+     *
+     * @param lists
+     * @return
+     */
+    public ListNode mergeKLists_v3(ListNode[] lists) {
+        if (lists == null || lists.length == 0) return null;
+        return merge(lists, 0, lists.length - 1);
+    }
+
+    private ListNode merge(ListNode[] lists, int left, int right) {
+        if (left == right) return lists[left];
+        int mid = left + (right - left) / 2;
+        ListNode l1 = merge(lists, left, mid);
+        ListNode l2 = merge(lists, mid + 1, right);
+        return mergeTwoLists(l1, l2);
+    }
+
+    private ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        if (l1 == null) return l2;
+        if (l2 == null) return l1;
+        if (l1.val < l2.val) {
+            l1.next = mergeTwoLists(l1.next, l2);
+            return l1;
+        } else {
+            l2.next = mergeTwoLists(l1, l2.next);
+            return l2;
+        }
+    }
+
+    /**
+     * 合并两个有序链表
+     *
+     * @param l1
+     * @param l2
+     * @return
+     */
+    public ListNode mergeTwoLists_v2(ListNode l1, ListNode l2) {
+        //哨兵，优化代码
+        ListNode head = new ListNode(0);
+        ListNode p = head;
+
+        while (l1 != null || l2 != null) {
+
+            if (l1.val < l2.val) {
+                p.next = l1;
+                l1 = l1.next;
+            } else {
+                p.next = l2;
+                l2 = l2.next;
+            }
+            p = p.next;
+
+            if (l1 == null) {
+                p.next = l2;
+                break;
+            }
+            if (l2 == null) {
+                p.next = l1;
+                break;
+            }
+        }
+        return head;
     }
 
     /**
