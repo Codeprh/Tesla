53044f37232dde7c65cda7a2f0e524c432b9af61
Noah Pan <codingprh@gmail.com>
2019-11-29 14:10:51 +0800

    代码提交

--- src/leetcode/_41.java
+++ src/leetcode/_41.java
@@ -1,5 +1,7 @@
 package leetcode;
 
+import java.util.Arrays;
+
 /**
  * 描述:
  * 未排序数组中，没有出现的最小正整数
@@ -20,7 +22,7 @@ public class _41 {
 
     public static void main(String[] args) {
         _41 app = new _41();
-        int[] nums = new int[]{7,8,9,11,12};
+        int[] nums = new int[]{1,2,0};
         int r = app.firstMissingPositive(nums);
         System.out.println("未排序数组中，没有出现的最小正整数=" + r);
     }
@@ -34,11 +36,31 @@ public class _41 {
     public int firstMissingPositive(int[] nums) {
 
         int minInt = 1;
+        Arrays.sort(nums);
 
+        boolean flag = false;
         for (int i = 0; i < nums.length; i++) {
+
+            //特殊处理1的请求
+            if (nums[i] == 1) {
+                flag = true;
+                minInt = 1;
+                break;
+            }
+
             if (nums[i] <= minInt && nums[i] > 0) {
                 minInt = ++nums[i];
             }
+        }
+
+        while (flag) {
+
+            ++minInt;
+            //说明存在
+            if (Arrays.binarySearch(nums, minInt) < 0) {
+                break;
+            }
+
         }
 
         return minInt;
