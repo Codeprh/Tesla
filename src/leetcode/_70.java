c4aca3164ac29bb73f947a25e378a43809cb9767
Noah Pan <codingprh@gmail.com>
2019-12-02 14:40:14 +0800

    代码提交

--- src/leetcode/_70.java
+++ src/leetcode/_70.java
@@ -15,24 +15,31 @@ package leetcode;
  */
 public class _70 {
     /**
-     * 递归版本
-     * todo：优化
+     * 非递归版本：最优解
+     *
+     * 解答成功:
+     * 执行耗时:0 ms,击败了100.00% 的Java用户
+     * 内存消耗:33.1 MB,击败了71.44% 的Java用户
      *
      * @param n
      * @return
      */
     public int climbStairs(int n) {
-        if (n == 1) {
-            return 1;
+        if (n == 1) return 1;
+        if (n == 2) return 2;
+        int ret = 0;
+        int pre = 2;
+        int prepre = 1;
+        for (int i = 3; i <= n; ++i) {
+            ret = pre + prepre;
+            prepre = pre;
+            pre = ret;
         }
-        if (n == 2) {
-            return 2;
-        }
-        return climbStairs(n - 1) + climbStairs(n - 2);
+        return ret;
     }
 
     public static void main(String[] args) {
         _70 app = new _70();
-        System.out.println("总共爬楼梯的方法=" + app.climbStairs(3));
+        System.out.println("总共爬楼梯的方法=" + app.climbStairs(5));
     }
 }
