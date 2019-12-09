be11e21aa221c51bd205b7c4f8daa434f4bbee9f
Noah Pan <codingprh@gmail.com>
2019-12-09 09:41:42 +0800

    代码提交

--- src/leetcode/_69.java
+++ src/leetcode/_69.java
@@ -10,7 +10,8 @@ package leetcode;
  */
 public class _69 {
     /**
-     * todo：check代码
+     * 第一版本：自己实现，无法求解
+     *
      * @param x
      * @return
      */
@@ -34,8 +35,36 @@ public class _69 {
         return mid;
     }
 
+    /**
+     * 参考版本：二分查找思想解决
+     *
+     * @param x
+     * @return
+     */
+    public int mySqrt_v2(int x) {
+        // 注意：针对特殊测试用例，例如 2147395599
+        // 要把搜索的范围设置成长整型
+        // 为了照顾到 0 把左边界设置为 0
+        long left = 0;
+        // # 为了照顾到 1 把右边界设置为 x // 2 + 1
+        long right = x / 2 + 1;
+        while (left < right) {
+            // 注意：这里一定取右中位数，如果取左中位数，代码会进入死循环
+            // long mid = left + (right - left + 1) / 2;
+            long mid = (left + right + 1) >>> 1;
+            long square = mid * mid;
+            if (square > x) {
+                right = mid - 1;
+            } else {
+                left = mid;
+            }
+        }
+        // 因为一定存在，因此无需后处理
+        return (int) left;
+    }
+
     public static void main(String[] args) {
         _69 app = new _69();
-        System.out.println("x的平方根=" + app.mySqrt(1));
+        System.out.println("x的平方根=" + app.mySqrt_v2(1));
     }
 }
