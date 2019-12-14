a2307f8c38dc8333a04b7f01e67b926c1094006c
Noah Pan <codingprh@gmail.com>
2019-12-14 21:02:25 +0800

    代码提交

---
+++ src/leetcode/_226.java
@@ -0,0 +1,38 @@
+package leetcode;
+
+/**
+ * 描述:
+ * 翻转二叉树
+ * <p>
+ * 输入：
+ *   4
+ * /   \
+ * 2     7
+ * / \   / \
+ * 1   3 6   9
+ * 输出：
+ *   4
+ * /   \
+ * 7     2
+ * / \   / \
+ * 9   6 3   1
+ *
+ * @author Noah
+ * @create 2019-12-14 21:00
+ */
+public class _226 {
+
+    public TreeNode invertTree(TreeNode root) {
+        return null;
+    }
+
+    class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+}
