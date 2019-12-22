73a7e4a34e69bf2a5370f61b8f612f0c0ef4dd96
Noah Pan <codingprh@gmail.com>
2019-12-22 10:58:50 +0800

    代码提交

---
+++ src/leetcode/TreeNode.java
@@ -0,0 +1,119 @@
+package leetcode;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+/**
+ * 描述:
+ * TreeNode，leetcode题目的int型的树
+ *
+ * @author Noah
+ * @create 2019-12-22 09:55
+ */
+public class TreeNode<T> {
+
+    public T val;
+    public TreeNode left;
+    public TreeNode right;
+
+    public TreeNode(T val) {
+        this.val = val;
+    }
+
+    /**
+     * 参考：{@link geektime_datastructure_algorithm._23.TreeNode}
+     * <p>
+     * 创建int的二叉查找树，链表存储&&非完全二叉树
+     * //     10
+     * //   /   \
+     * //   9    14
+     * //       /   \
+     * //       13  16
+     * //       /
+     * //       11
+     *
+     * @return
+     */
+    public static <T> TreeNode creatBinarySearchTreesByLinked(boolean isInt) {
+        TreeNode _10 = new TreeNode(10);
+        TreeNode _9 = new TreeNode(9);
+        TreeNode _14 = new TreeNode(14);
+
+        TreeNode _13 = new TreeNode(13);
+        TreeNode _16 = new TreeNode(16);
+        TreeNode _11 = new TreeNode(11);
+
+        _10.left = _9;
+        _10.right = _14;
+
+        _14.left = _13;
+        _14.right = _16;
+
+        _13.left = _11;
+
+        return _10;
+    }
+
+    /**
+     * todo:创建二叉查找树，数组存储&&完全二叉树
+     *
+     * @param <T>
+     * @return
+     */
+    public static <T> TreeNode creatBinarySearchTreesByArray(boolean isInt) {
+        return null;
+    }
+
+    /**
+     * todo:创建满二叉树，链表存储
+     *
+     * @param isInt
+     * @param <T>
+     * @return
+     */
+    public static <T> TreeNode createFullBinaryTreesByLinked(boolean isInt) {
+        return null;
+
+    }
+
+    /**
+     * 层次遍历
+     *
+     * @param root
+     * @return
+     */
+    public List<List<Integer>> LevelTraversal(TreeNode root) {
+        List<List<Integer>> levels = new ArrayList<List<Integer>>();
+        if (root == null) return levels;
+
+        Queue<TreeNode> queue = new LinkedList<TreeNode>();
+        queue.add(root);
+        int level = 0;
+
+        while (!queue.isEmpty()) {
+            // start the current level
+            levels.add(new ArrayList<Integer>());
+
+            // number of elements in the current level
+            int level_length = queue.size();
+            for (int i = 0; i < level_length; ++i) {
+                TreeNode node = queue.remove();
+
+                // fulfill the current level
+                levels.get(level).add((int) node.val);
+
+                // add child nodes of the current level
+                // in the queue for the next level
+                if (node.left != null) queue.add(node.left);
+                if (node.right != null) queue.add(node.right);
+            }
+            // go to next level
+            level++;
+        }
+        return levels;
+    }
+
+
+}
--- src/leetcode/_98.java
+++ src/leetcode/_98.java
@@ -11,41 +11,75 @@ package leetcode;
  * 节点的左子树只包含小于当前节点的数。
  * 节点的右子树只包含大于当前节点的数。
  * 所有左子树和右子树自身必须也是二叉搜索树。
- *
+ * <p>
  * 输入:
- *     2
- *    / \
- *   1   3
+ * 2
+ * / \
+ * 1   3
  * 输出: true
- *
+ * <p>
  * 输入:
- *     5
- *    / \
- *   1   4
- *      / \
- *     3   6
+ * 5
+ * / \
+ * 1   4
+ * / \
+ * 3   6
  * 输出: false
+ *
  * @author Noah
  * @create 2019-12-15 21:33
  */
 public class _98 {
+
+    public static void main(String[] args) {
+        TreeNode treeNode = TreeNode.creatBinarySearchTreesByLinked(false);
+
+        _98 app = new _98();
+        System.out.println("当前二叉树是查找树吗？" + app.isValidBST(treeNode));
+    }
+
     /**
-     * todo：代码实现
+     * 验证是一棵二叉查找树：1、根节点>左子树&&根节点<右子树
+     * <p>
+     * //采用递归实现
+     *
      * @param root
      * @return
      */
     public boolean isValidBST(TreeNode root) {
+
+
+        return helper(root, null, null);
+    }
+
+    public boolean helper(TreeNode node, Integer lower, Integer upper) {
+        if (node == null) return true;
+
+        int val = (int) node.val;
+        if (lower != null && val <= lower) return false;
+        if (upper != null && val >= upper) return false;
+
+        if (!helper(node.right, val, upper)) return false;
+        if (!helper(node.left, lower, val)) return false;
+        return true;
+    }
+
+
+    /**
+     * todo
+     * 中序遍历，等到的是一个有序的数组
+     *
+     * @param root
+     * @return
+     */
+    public boolean isValidBST_inorder_traversal(TreeNode root) {
+
+        if (root == null) {
+
+        }
         return false;
     }
 
-    class TreeNode {
-        int val;
-        TreeNode left;
-        TreeNode right;
 
-        TreeNode(int x) {
-            val = x;
-        }
-    }
 }
 
