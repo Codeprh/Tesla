47b4c1c15f2ff53b9111df46e66872d3165db62c
Noah Pan <codingprh@gmail.com>
2019-12-27 09:37:42 +0800

    代码提交

---
+++ src/swordFingerOffer/_22.java
@@ -0,0 +1,30 @@
+package swordFingerOffer;
+
+/**
+ * 描述:
+ * 输入一个链表，输出该链表中倒数第k个结点。
+ *
+ * @author Noah
+ * @create 2019-12-26 09:54
+ */
+public class _22 {
+    /**
+     * 计算链表倒数第K个节点
+     *
+     * @param head
+     * @param k
+     * @return
+     */
+    public ListNode FindKthToTail(ListNode head, int k) {
+        return null;
+    }
+
+    class ListNode {
+        int val;
+        ListNode next = null;
+
+        ListNode(int val) {
+            this.val = val;
+        }
+    }
+}
