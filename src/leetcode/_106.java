8ce755664f9a57b5861d015a8ea26f5b9c081db3
Noah Pan <codingprh@gmail.com>
2020-01-15 17:04:02 +0800

    代码提交

--- src/leetcode/_106.java
+++ src/leetcode/_106.java
@@ -34,6 +34,31 @@ public class _106 {
 
     }
 
+    /**
+     * 参考版本
+     */
+    HashMap<Integer, Integer> memo = new HashMap<>();
+    int[] post;
+
+    public TreeNode buildTree_v2(int[] inorder, int[] postorder) {
+        for (int i = 0; i < inorder.length; i++) memo.put(inorder[i], i);
+        post = postorder;
+        TreeNode root = buildTree(0, inorder.length - 1, 0, post.length - 1);
+        return root;
+    }
+
+    public TreeNode buildTree(int is, int ie, int ps, int pe) {
+        if (ie < is || pe < ps) return null;
+
+        int root = post[pe];
+        int ri = memo.get(root);
+
+        TreeNode node = new TreeNode(root);
+        node.left = buildTree(is, ri - 1, ps, ps + ri - is - 1);
+        node.right = buildTree(ri + 1, ie, ps + ri - is, pe - 1);
+        return node;
+    }
+
     //=======================
     //=========错误自己实现===
     //=======================
